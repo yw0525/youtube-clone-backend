bf5ed9d73f1c03b51ed750cbc4649b7c267c1fc8
heora <yueluo.yang@gmail.com>
2022-10-05 10:32:04 +0800

    feat: add user login api

--- app/controller/user.ts
+++ app/controller/user.ts
@@ -41,4 +41,48 @@ export default class UserController extends Controller {
       }
     }
   }
+
+  async login() {
+    const { ctx } = this
+    const { body } = ctx.request
+
+    // 1. params validate
+    ctx.validate(
+      {
+        email: { type: 'email' },
+        password: { type: 'string' }
+      },
+      body
+    )
+
+    const userService = this.service.user
+    const { username, email, password } = body
+
+    // 2. data validate
+    const user = await userService.findByEmail(email)
+
+    if (!user) {
+      ctx.throw(422, '用户不存在')
+    }
+
+    if (ctx.helper.md5(password) !== user.password) {
+      ctx.throw(422, '密码不正确')
+    }
+
+    // 3. generate token
+    const token = userService.createToken({
+      userId: user._id
+    })
+
+    // 4. send response
+    ctx.body = {
+      user: {
+        email: user.email,
+        token,
+        username,
+        channelDescription: user.channelDescription,
+        avatar: user.avatar
+      }
+    }
+  }
 }
--- app/router.ts
+++ app/router.ts
@@ -3,6 +3,10 @@ import { Application } from 'egg'
 export default (app: Application) => {
   const { controller, router } = app
 
+  router.prefix('/api/v1')
+
   router.get('/', controller.home.index)
-  router.post('/user/create', controller.user.create)
+
+  router.post('/users/create', controller.user.create)
+  router.post('/users/login', controller.user.login)
 }
